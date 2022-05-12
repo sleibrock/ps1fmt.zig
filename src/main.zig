945001b37ffcd3d60f718b9f1e56df453bcac07b
Steven L <steven.leibrock@gmail.com>
2022-05-12 15:37:13 -0400

    Using tagged enum instead of two separate enums

--- src/main.zig
+++ src/main.zig
@@ -4,14 +4,23 @@ const debug = std.debug;
 const os = std.os;
 const ArrayList = std.ArrayList;
 
-// figure out a way to represent ansi signals/vars
+// Represent the difference between value and state
+// value is result, state is in transition
+const Tag = enum { value, state };
+
+// apply the Tag enum to a proper union
+const Result = union(Tag) { value: ANSI, state: StateM };
+
+// Our way of representing ANSI escape sequences and actions
+// or variables for substitution purposes
+// before_seq and after_seq match each other,
+// and end will terminate any action sequences before it
 const ANSI = enum {
     none,
-    open,
-    close,
+    action,
+    end,
     before_seq,
     after_seq,
-    terminator,
 
     // time/date
     date_dmy,
@@ -53,10 +62,9 @@ const ANSI = enum {
     is_root,
 };
 
-// state machine type representing the current state
-// of the parser
+// state machine type representing multiple parser states
 const StateM = enum {
-    none,
+    none, // indicate no progress or invalidity
     begin, //\
     byte1, //0
     byte2, //3
@@ -69,34 +77,6 @@ const StateM = enum {
     byte9, //w
     byte10, //v
     byte11, //m end of sequence
-
-    // openers, closers, or terminators
-    terminator, // end, or \\[
-    opener, // signal that starts a sequence
-    closer, // signal that ends an opener
-    before_seq, // \[
-    after_seq, // \]
-
-    // variables to match the ANSI enum
-    var_date_dmy,
-    var_time12h,
-    var_time12a,
-    var_time24h,
-    var_pathshort,
-    var_pathfull,
-    var_newline,
-    var_carriage_ret,
-    var_jobs,
-    var_username,
-    var_short_hostname,
-    var_full_hostname,
-    var_termdevice,
-    var_shell_name,
-    var_shell_version,
-    var_shell_version_plvl,
-    var_command_pos,
-    var_command_number,
-    var_is_root,
 };
 
 // number ascii range is from 48 to 57
@@ -112,198 +92,115 @@ pub fn is_num(x: u8) bool {
 // Very code heavy, but easy to add new features each step
 // of the way.
 // Basically a much longer Haskell function
-pub fn next_state(S: StateM, x: u8) StateM {
+pub fn next_state(S: StateM, x: u8) Result {
     switch (S) {
         StateM.none => {
             if (x == '\\')
-                return StateM.begin;
+                return .{ .state = StateM.begin };
         },
         StateM.begin => {
             // if it's \e, interpret as an escape code
             // and move up to the command sequence stage
             if (x == 'e')
-                return StateM.byte3;
+                return .{ .state = StateM.byte3 };
 
             // check for single-char ansi codes
             // these are word wrap markers
             if (x == '[')
-                return StateM.before_seq;
+                return .{ .value = ANSI.before_seq };
             if (x == ']')
-                return StateM.after_seq;
+                return .{ .value = ANSI.after_seq };
 
             // these are PSX fill-in variables
             if (x == 'd')
-                return StateM.var_date_dmy;
+                return .{ .value = ANSI.date_dmy };
             if (x == 'T')
-                return StateM.var_time12h;
+                return .{ .value = ANSI.time12h };
             if (x == '@')
-                return StateM.var_time12a;
+                return .{ .value = ANSI.time12a };
             if (x == 'w')
-                return StateM.var_pathshort;
+                return .{ .value = ANSI.path_short };
             if (x == 'W')
-                return StateM.var_pathfull;
+                return .{ .value = ANSI.path_full };
             if (x == 'n')
-                return StateM.var_newline;
+                return .{ .value = ANSI.new_line };
             if (x == 'r')
-                return StateM.var_carriage_ret;
+                return .{ .value = ANSI.carriage_ret };
             if (x == 'u')
-                return StateM.var_username;
+                return .{ .value = ANSI.username };
             if (x == 'h')
-                return StateM.var_short_hostname;
+                return .{ .value = ANSI.short_hostname };
             if (x == 'H')
-                return StateM.var_full_hostname;
+                return .{ .value = ANSI.full_hostname };
             if (x == 'j')
-                return StateM.var_jobs;
+                return .{ .value = ANSI.jobs };
             if (x == 'l')
-                return StateM.var_termdevice;
+                return .{ .value = ANSI.termdevice };
             if (x == 's')
-                return StateM.var_shell_name;
+                return .{ .value = ANSI.shell_name };
             if (x == 'v')
-                return StateM.var_shell_version;
+                return .{ .value = ANSI.shell_version };
             if (x == 'V')
-                return StateM.var_shell_version_plvl;
+                return .{ .value = ANSI.shell_version_plvl };
             if (x == '!')
-                return StateM.var_command_pos;
+                return .{ .value = ANSI.command_pos };
             if (x == '#')
-                return StateM.var_command_number;
+                return .{ .value = ANSI.command_number };
             if (x == '$')
-                return StateM.var_is_root;
+                return .{ .value = ANSI.is_root };
 
             // absorb next 3 numbers for ascii escape
             if (is_num(x))
-                return StateM.byte1;
+                return .{ .state = StateM.byte1 };
         },
         StateM.byte1 => {
             if (is_num(x))
-                return StateM.byte2;
+                return .{ .state = StateM.byte2 };
         },
         StateM.byte2 => {
             if (is_num(x))
-                return StateM.byte3;
+                return .{ .state = StateM.byte3 };
         },
         StateM.byte3 => {
             if (x == '[')
-                return StateM.byte4;
+                return .{ .state = StateM.byte4 };
         },
         StateM.byte4 => {
             if (is_num(x))
-                return StateM.byte5;
+                return .{ .state = StateM.byte5 };
         },
         StateM.byte5 => {
             // the first opcode can be one or two digits
             // so if it's a number, move on, else jump further
             if (is_num(x))
-                return StateM.byte6;
+                return .{ .state = StateM.byte6 };
             if (x == ';')
-                return StateM.byte7;
+                return .{ .state = StateM.byte7 };
         },
         StateM.byte6 => {
             // is either ; or m
             if (x == 'm')
-                return StateM.closer;
+                return .{ .value = ANSI.end };
             if (x == ';')
-                return StateM.byte7;
+                return .{ .state = StateM.byte7 };
         },
         StateM.byte7 => {
             if (is_num(x))
-                return StateM.byte8;
+                return .{ .state = StateM.byte8 };
         },
         StateM.byte8 => {
             if (is_num(x))
-                return StateM.byte9;
+                return .{ .state = StateM.byte9 };
         },
         StateM.byte9 => {
             if (x == 'm')
-                return StateM.opener;
+                return .{ .value = ANSI.action };
         },
         else => {
-            return StateM.none;
+            return .{ .state = StateM.none };
         },
     }
-    return StateM.none;
-}
-
-// Convert a StateM enum to an ANSI enum
-// Instead of bundling up the final steps
-// in main, let's use this to convert
-// base case: no signal match and it gives us ANSI.none
-pub fn statem_to_ansi(S: StateM) ANSI {
-    switch (S) {
-        StateM.terminator => {
-            return ANSI.terminator;
-        },
-        StateM.before_seq => {
-            return ANSI.before_seq;
-        },
-        StateM.after_seq => {
-            return ANSI.after_seq;
-        },
-        StateM.opener => {
-            return ANSI.open;
-        },
-        StateM.closer => {
-            return ANSI.close;
-        },
-        StateM.var_date_dmy => {
-            return ANSI.date_dmy;
-        },
-        StateM.var_time12h => {
-            return ANSI.time12h;
-        },
-        StateM.var_time12a => {
-            return ANSI.time12a;
-        },
-        StateM.var_time24h => {
-            return ANSI.time24h;
-        },
-        StateM.var_username => {
-            return ANSI.username;
-        },
-        StateM.var_short_hostname => {
-            return ANSI.short_hostname;
-        },
-        StateM.var_full_hostname => {
-            return ANSI.full_hostname;
-        },
-        StateM.var_pathshort => {
-            return ANSI.path_short;
-        },
-        StateM.var_pathfull => {
-            return ANSI.path_full;
-        },
-        StateM.var_newline => {
-            return ANSI.new_line;
-        },
-        StateM.var_carriage_ret => {
-            return ANSI.carriage_ret;
-        },
-        StateM.var_jobs => {
-            return ANSI.jobs;
-        },
-        StateM.var_termdevice => {
-            return ANSI.termdevice;
-        },
-        StateM.var_shell_name => {
-            return ANSI.shell_name;
-        },
-        StateM.var_shell_version => {
-            return ANSI.shell_version;
-        },
-        StateM.var_shell_version_plvl => {
-            return ANSI.shell_version_plvl;
-        },
-        StateM.var_command_pos => {
-            return ANSI.command_pos;
-        },
-        StateM.var_command_number => {
-            return ANSI.command_number;
-        },
-        StateM.var_is_root => {
-            return ANSI.is_root;
-        },
-        else => {},
-    }
-    return ANSI.none;
+    return .{ .state = StateM.none };
 }
 
 // Main function stub, glues logic together
@@ -331,18 +228,16 @@ pub fn main() !void {
 
     // iter over each char in PS1 str
     // note: `ps1.?` forces the optional `orelse` syntax
-    var state = StateM.none;
-    var result: ANSI = ANSI.none;
+    // updated: now uses a tagged union dispatch
+    // so we're not duplicating enum types everywhere!
+    var result = StateM.none;
     for (ps1.?) |char| {
-        // update the state to the next one by feeding it the next char
-        state = next_state(state, char);
-
-        // if we found a final state, append it to our list
-        result = statem_to_ansi(state);
-        if (result != ANSI.none) {
-            try alist.append(result);
-            result = ANSI.none;
-            state = StateM.none;
+        switch (next_state(result, char)) {
+            .state => |s| result = s,
+            .value => |v| {
+                try alist.append(v);
+                result = StateM.none;
+            },
         }
     }
 
@@ -353,6 +248,9 @@ pub fn main() !void {
     for (alist.items) |element| {
         debug.print("{s}\n", .{element});
     }
+
+    // do some kind of iteration w/ warnings
+
     debug.print("\nGoodbye\n", .{});
 }
 
