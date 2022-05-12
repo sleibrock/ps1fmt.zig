bbb5cec95cc5ec023f298b18571a3f0455d9878c
Steven L <steven.leibrock@gmail.com>
2022-05-12 15:55:09 -0400

    Adding helpful build options to build.zig

--- build.zig
+++ build.zig
@@ -2,10 +2,12 @@ const Builder = @import("std").build.Builder;
 
 pub fn build(b: *Builder) void {
     const target = b.standardTargetOptions(.{});
+    b.setPreferredReleaseMode(.ReleaseSmall);
     const mode = b.standardReleaseOptions();
     const exe = b.addExecutable("ps1fmt", "src/main.zig");
     exe.setTarget(target);
     exe.setBuildMode(mode);
+    exe.strip = true;
     exe.install();
 
     const run_cmd = exe.run();
